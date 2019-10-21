3b0a70b5d5a9847bf8aff5eae4ef70dad9b59ace
BennyLouie <bennylouie1412@gmail.com>
2019-10-21 17:55:52 -0400

    added js for displaying profile

---
+++ index.js
@@ -0,0 +1,114 @@
+let selects = document.querySelector(".pokemon-dropdown-select")
+let pokemonProfile = document.querySelector('.pokemon-post-container')
+
+let displayForm = false
+
+get("http://localhost:3000/pokemons")
+.then(respJSON => {
+    respJSON.forEach(opt => {
+        let newOption = document.createElement('option')
+        newOption.className = 'selected'
+        newOption.value = opt[0]
+        newOption.innerText = opt[1]
+        selects.append(newOption)
+    })
+})
+
+selects.addEventListener('change', evt => {
+    evt.preventDefault()
+    let selected = evt.target.value
+    get(`http://localhost:3000/pokemons/${selected}`)
+    .then(respJSON => {
+        createProfile(respJSON)
+    })
+})
+
+function removeChildren(parentNode) {  
+    while (parentNode.firstChild) { 
+      parentNode.removeChild(parentNode.firstChild)  
+    }
+  }
+
+function get(url){
+return fetch(url)
+.then(resp => resp.json())
+}
+
+function createProfile(obj){
+    
+    removeChildren(pokemonProfile)
+    
+    let profileCard = document.createElement('div')
+        let profileName = document.createElement('h3')
+            profileName.innerText = `${obj.species}`
+            profileName.className = 'pokemon-name'
+
+        let followerCount = document.createElement('h4')
+            followerCount.className = 'follower-count'
+            followerCount.innerText = ' followers'
+
+            let countSpan = document.createElement('span')
+                countSpan.innerText = `${obj.followers.length}`
+            followerCount.prepend(countSpan)
+
+        let profilePic = document.createElement('img')
+            profilePic.src = `${obj.image}`
+            profilePic.alt = `${obj.species}-pic`
+            profilePic.height = '100'
+            profilePic.width = '100'
+
+        let formContainer = document.createElement('div')
+            formContainer.className = 'form-container'
+
+            let createPostForm = document.createElement('form')
+                createPostForm.className = 'create-post-form'
+
+                let formHeader = document.createElement('h3')
+                    formHeader.innerText = 'New Post'
+
+                let captionInput = document.createElement('input')
+                    captionInput.type = 'text'
+                    captionInput.value = ""
+                    captionInput.placeholder = "Caption..."
+                    captionInput.className = "input-text"
+                
+                let br = document.createElement('br')
+
+                let imageInput = document.createElement('input')
+                    imageInput.type = 'text'
+                    imageInput.value = ""
+                    imageInput.placeholder = "Image URL..."
+                    imageInput.className = 'input-text'
+
+                let submitForm = document.createElement('input')
+                    submitForm.type = 'submit'
+                    submitForm.name = 'submit'
+                    submitForm.value = 'Submit Post'
+                    submitForm.className = 'submit'
+
+            createPostForm.append(formHeader, captionInput, br, imageInput, br, submitForm)
+
+        formContainer.append(createPostForm)
+
+        //Still need to work on toggling the form to display-------------------------
+        let toggleForm = document.createElement('p')
+            // toggleForm.style = 'text-align: center'
+            toggleForm.innerText = '+ Post'
+            toggleForm.addEventListener('click', evt => {
+                formDisplay(toggleForm)
+            })
+
+    profileCard.append(profileName, followerCount, profilePic, formContainer, toggleForm)
+    pokemonProfile.append(profileCard)
+}
+
+//This toggle function still needs work---------------------------------------
+function formDisplay(toggleForm){
+    displayForm = !displayForm
+
+    if (displayForm){
+        toggleForm.style.display = 'block'
+    } else {
+        toggleForm.style.display = 'none'
+    }
+}
